b54b45e73de6fb2a28c69d7223c9cbb6025b6d58
Michail-DM <82368280+Michail-DM@users.noreply.github.com>
2022-01-25 13:52:59 +0300

    Add files via upload

---
+++ DMA14Glava/DMA14Glava/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿
+namespace DMA14Glava
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboCulture = new System.Windows.Forms.ComboBox();
+            this.lstMonth = new System.Windows.Forms.ListBox();
+            this.lstWeek = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // cboCulture
+            // 
+            this.cboCulture.FormattingEnabled = true;
+            this.cboCulture.Location = new System.Drawing.Point(221, 124);
+            this.cboCulture.Name = "cboCulture";
+            this.cboCulture.Size = new System.Drawing.Size(121, 21);
+            this.cboCulture.TabIndex = 0;
+            this.cboCulture.SelectedIndexChanged += new System.EventHandler(this.cboCulture_SelectedIndexChanged);
+            // 
+            // lstMonth
+            // 
+            this.lstMonth.FormattingEnabled = true;
+            this.lstMonth.Location = new System.Drawing.Point(105, 215);
+            this.lstMonth.Name = "lstMonth";
+            this.lstMonth.Size = new System.Drawing.Size(120, 95);
+            this.lstMonth.TabIndex = 1;
+            // 
+            // lstWeek
+            // 
+            this.lstWeek.FormattingEnabled = true;
+            this.lstWeek.Location = new System.Drawing.Point(320, 215);
+            this.lstWeek.Name = "lstWeek";
+            this.lstWeek.Size = new System.Drawing.Size(120, 95);
+            this.lstWeek.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstWeek);
+            this.Controls.Add(this.lstMonth);
+            this.Controls.Add(this.cboCulture);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboCulture;
+        private System.Windows.Forms.ListBox lstMonth;
+        private System.Windows.Forms.ListBox lstWeek;
+    }
+}
+
---
+++ DMA14Glava/DMA14Glava/Form2.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;
+using System.Threading;
+namespace DMA14Glava
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            TreeNode tempNode;
+            // Добавляем к дереву нейтральные культуры, как корневые узлы
+            foreach (CultureInfo CultureX in CultureInfo.GetCultures(CultureTypes.NeutralCultures))
+            {
+                tempNode = new TreeNode(CultureX.EnglishName
+                + " [" + CultureX.Name + "]");
+                tempNode.Tag = CultureX;
+                treeView1.Nodes.Add(tempNode);
+            }
+            // Перебираем все конкретные культуры и добавляем
+            // каждую из них к родительской нейтральной культуре в дереве
+            foreach (CultureInfo CultureX in
+            CultureInfo.GetCultures(CultureTypes.SpecificCultures))
+            {
+                foreach (TreeNode NodeX in treeView1.Nodes)
+                {
+                    if (NodeX.Tag.Equals(CultureX.Parent))
+                    {
+                        tempNode = new TreeNode(CultureX.EnglishName
+                        + " [" + CultureX.Name + "]");
+                        tempNode.Tag = CultureX;
+                        tempNode.ForeColor = Color.Red;
+                        foreach (CultureInfo CultureY in CultureInfo.GetCultures(
+                        CultureTypes.InstalledWin32Cultures))
+                        {
+                            if (CultureY.Equals(CultureX))
+                            {
+                                tempNode.ForeColor = Color.Black;
+                                break;
+                            }
+                        }
+                        NodeX.Nodes.Add(tempNode);
+                        break;
+                    }
+                }
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void treeView1_AfterSelect(object sender, System.Windows.Forms.TreeViewEventArgs e)
+        
+                {
+                    CultureInfo selectedCulture;
+                    selectedCulture = (CultureInfo)treeView1.SelectedNode.Tag;
+                    if (selectedCulture.IsNeutralCulture == true)
+                    {
+                        listView1.Items[3].SubItems[1].Text = "Нейтральная";
+                        for (int x = 4; x <= 10; x++)
+                            listView1.Items[x].SubItems[1].Text = "Нейтральная культура";
+                    }
+                    else
+                    {
+                        Thread.CurrentThread.CurrentCulture = selectedCulture;
+                        listView1.Items[3].SubItems[1].Text = "Конкретная";
+                        listView1.Items[4].SubItems[1].Text =
+                        (DateTime.Now).ToShortDateString();
+                        listView1.Items[5].SubItems[1].Text =
+                        (DateTime.Now).ToLongDateString();
+                        listView1.Items[6].SubItems[1].Text =
+                        (DateTime.Now).ToShortTimeString();
+                        listView1.Items[7].SubItems[1].Text =
+                        (DateTime.Now).ToLongTimeString();
+                        listView1.Items[8].SubItems[1].Text = (35500.75).ToString("n");
+                        listView1.Items[9].SubItems[1].Text = (1750.25).ToString("c");
+                        listView1.Items[10].SubItems[1].Text =
+                        (selectedCulture.Calendar.ToString()).Remove(0, 21);
+                    }
+                    listView1.Items[0].SubItems[1].Text = selectedCulture.Name;
+                    listView1.Items[1].SubItems[1].Text =
+     selectedCulture.EnglishName;
+                    listView1.Items[2].SubItems[1].Text = selectedCulture.NativeName;
+                }
+            }
+        }
+   
